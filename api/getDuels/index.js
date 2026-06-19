7245d184c90d451b1a9c30d43fca7989e57d2079
aPaddes <admin@Paddes.onmicrosoft.com>
2026-06-19 14:18:12 +0200

    Implement getDuels API endpoint with season query

--- api/getDuels/index.js
+++ api/getDuels/index.js
@@ -1,1 +1,13 @@
+const { duels } = require("../shared/cosmos");
 
+module.exports = async function (context, req) {
+  const season = req.query.season || "2026-S1";
+
+  const querySpec = {
+    query: "SELECT * FROM c WHERE c.season = @season ORDER BY c.date DESC",
+    parameters: [{ name: "@season", value: season }]
+  };
+
+  const { resources } = await duels.items.query(querySpec).fetchAll();
+  context.res = { status: 200, body: resources };
+};

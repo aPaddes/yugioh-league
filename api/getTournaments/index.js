0d86ff90a25e3d461ca2594dab722f55691a9147
aPaddes <admin@Paddes.onmicrosoft.com>
2026-06-19 14:20:07 +0200

    Update index.js

--- api/getTournaments/index.js
+++ api/getTournaments/index.js
@@ -1,1 +1,13 @@
+const { tournaments } = require("../shared/cosmos");
 
+module.exports = async function (context, req) {
+  const season = req.query.season || "2026-S1";
+
+  const querySpec = {
+    query: "SELECT * FROM c WHERE c.season = @season ORDER BY c.createdAt DESC",
+    parameters: [{ name: "@season", value: season }]
+  };
+
+  const { resources } = await tournaments.items.query(querySpec).fetchAll();
+  context.res = { status: 200, body: resources };
+};

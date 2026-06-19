a255d9e6fb6f3853840a84c5d156f2143db86f7b
aPaddes <admin@Paddes.onmicrosoft.com>
2026-06-19 14:14:05 +0200

    Implement Elo rating calculation functions

--- api/shared/elo.js
+++ api/shared/elo.js
@@ -1,1 +1,17 @@
+const K = 32;
 
+function expected(a, b) {
+  return 1 / (1 + Math.pow(10, (b - a) / 400));
+}
+
+function calculate(winnerElo, loserElo) {
+  const ew = expected(winnerElo, loserElo);
+  const el = expected(loserElo, winnerElo);
+
+  return {
+    winner: Math.round(winnerElo + K * (1 - ew)),
+    loser: Math.round(loserElo + K * (0 - el))
+  };
+}
+
+module.exports = { calculate };

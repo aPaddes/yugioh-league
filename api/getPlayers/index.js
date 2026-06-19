59e93616c5dd27d52c30e960d6bd94e7a7e3af9f
aPaddes <admin@Paddes.onmicrosoft.com>
2026-06-19 14:17:02 +0200

    Fetch and sort players by elo in getPlayers API

--- api/getPlayers/index.js
+++ api/getPlayers/index.js
@@ -1,1 +1,7 @@
+const { players } = require("../shared/cosmos");
 
+module.exports = async function (context) {
+  const { resources } = await players.items.readAll().fetchAll();
+  resources.sort((a, b) => b.elo - a.elo);
+  context.res = { status: 200, body: resources };
+};

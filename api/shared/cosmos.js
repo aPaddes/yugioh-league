8532f016834754a61ff05c2381f75d7f3d972d06
aPaddes <admin@Paddes.onmicrosoft.com>
2026-06-19 14:13:37 +0200

    Update cosmos.js

--- api/shared/cosmos.js
+++ api/shared/cosmos.js
@@ -1,1 +1,12 @@
+const { CosmosClient } = require("@azure/cosmos");
 
+const client = new CosmosClient(process.env.COSMOS_CONNECTION_STRING);
+const db = client.database("yugiohleague");
+
+module.exports = {
+  db,
+  players: db.container("players"),
+  duels: db.container("duels"),
+  tournaments: db.container("tournaments"),
+  eloHistory: db.container("eloHistory")
+};
